d7eab5945728de6f54c3ae5965f4957cfb183404
Eric Kivel <ericvkivel1@gmail.com>
2023-10-28 16:33:09 -0300

    refactor: add cpu frequency reset

--- run.sh
+++ run.sh
@@ -2,25 +2,29 @@
 
 # Eric Kivel - GRR20220069 | Murilo Paes - GRR20190158
 
-METRICA="FLOPS_DP ENERGY" 
+METRICA="FLOPS_DP ENERGY MEM CACHE" 
 
 make
 
+echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
 for k in $METRICA
 do
-  likwid-perfctr -C 3 -g ${k} -m ./ajustePol < pontos.in > ${k}_resultado.log
+  likwid-perfctr -C 3 -g ${k} -m ./matmult $1 > ${k}_resultado.log
 done
 
+echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
 printf "Resultado: \n" > resultado.out
-./ajustePol < pontos.in >> resultado.out
+./matmult $1 >> resultado.out
 printf "\n" >> resultado.out
 
-echo "Build Linear System" >> resultado.out
+echo "Matrix x Matrix" >> resultado.out
 cat FLOPS_DP_resultado.log | grep -m 1 "DP MFLOP/s" >> resultado.out
 cat FLOPS_DP_resultado.log | grep -m 1 "DP MFLOP/s" >> resultado.out
 cat ENERGY_resultado.log | grep -m 1 "Energy Core" >> resultado.out
 printf "\n" >> resultado.out
-echo "Solve Linear System" >> resultado.out
+echo "Matrix x Vector" >> resultado.out
 cat FLOPS_DP_resultado.log | grep -m 2 "DP MFLOP/s" | tail -n 1 >> resultado.out
 cat FLOPS_DP_resultado.log | grep -m 2 "DP MFLOP/s" | tail -n 1 >> resultado.out
 cat ENERGY_resultado.log | grep -m 2 "Energy Core" | tail -n 1 >> resultado.out
